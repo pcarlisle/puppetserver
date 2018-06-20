dc586ac6fb3934ab7cf39f75c0e18bb5b1e097ff
Patrick Carlisle <patrick@puppetlabs.com>
2018-06-20 12:46:34 -0700

    Add java arguments to bring in modules we use under Java 9+
    Project Jigsaw separated these out so they are not visible by defaultd. We
    depend on code that expects them to be present.

--- project.clj
+++ project.clj
@@ -231,7 +231,10 @@
   :jvm-opts ["-Djruby.logger.class=com.puppetlabs.jruby_utils.jruby.Slf4jLogger"
              "-XX:+UseG1GC"
              ~(str "-Xms" (heap-size "1G" "min"))
-             ~(str "-Xmx" (heap-size "2G" "max"))]
+             ~(str "-Xmx" (heap-size "2G" "max"))
+             "-XX:+IgnoreUnrecognizedVMOptions"
+             "--add-modules" "java.xml.bind"
+             "--add-modules" "java.xml.ws"]
 
   :repl-options {:init-ns dev-tools}
 
